9f37cc94699823fa91d95aa90350afb659e24f76
unknown <basdemir35@hotmail.com.tr>
2024-08-20 17:50:56 +0300

    added asynchronous messaging support for platform publishing
    - Injected IMessageBusClient into the PlatformsController to enable communication with the message bus.
    - Added logic in the CreatePlatform method to publish the newly created platform data asynchronously via the message bus.
    - The platform data is transformed into a PlatformPublishedDto, which includes an event field set to 'Platform_Published'.
    - Wrapped the asynchronous message publishing inside a try-catch block to handle potential connection failures, ensuring the service gracefully handles any issues with the message bus.

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using PlatformService.AsyncDataServices;
 using PlatformService.Data;
 using PlatformService.Dtos;
 using PlatformService.Models;
@@ -14,12 +15,14 @@ namespace PlatformService.Controllers
         private readonly IPlatformRepo _repository;
         private readonly IMapper _mapper;
         private readonly ICommandDataClient _commandDataClient;
+        private readonly IMessageBusClient _messageBusClient;
 
-        public PlatformsController(IPlatformRepo repository, IMapper mapper, ICommandDataClient commandDataClient)
+        public PlatformsController(IPlatformRepo repository, IMapper mapper, ICommandDataClient commandDataClient, IMessageBusClient messageBusClient)
         {
             _repository = repository;
             _mapper = mapper;
             _commandDataClient = commandDataClient;
+            _messageBusClient = messageBusClient;
         }
 
         [HttpGet]
@@ -54,6 +57,7 @@ namespace PlatformService.Controllers
 
             var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
 
+            // Send Sync Message
             try
             {
                 await _commandDataClient.SendPlatformToCommand(platformReadDto);
@@ -62,6 +66,18 @@ namespace PlatformService.Controllers
             {
                 Console.WriteLine($"---> Could not send synchronously: {ex.Message}");
             };
+
+            // Send Async Message
+            try
+            {
+                var platformPusblishedDto = _mapper.Map<PlatformPublishedDto>(platformReadDto);
+                platformPusblishedDto.Event = "Platform_Published";
+                _messageBusClient.PublishNewPlatform(platformPusblishedDto);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"---> Could not send asynchronously: {ex.Message}");
+            }
 
             // The 'CreatedAtRoute' method generates the URL of the newly created resource using the 'GetPlatformById' route name.
             // 'nameof(GetPlatformById)' specifies the route name for generating the URL.
