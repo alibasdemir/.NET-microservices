eeef3aa6b24e2fbb72b75fe58058b27c613930b5
unknown <basdemir35@hotmail.com.tr>
2024-08-23 19:37:19 +0300

    add gRPC service for retrieving platforms
    - Created `GrpcPlatformService` to handle gRPC requests. This service uses `IPlatformRepo` to retrieve platform data from the repository and `AutoMapper` to map the data to the gRPC model.
    - Implemented `GetAllPlatforms` method, which returns a list of platforms as a `PlatformResponse`.
    - Added AutoMapper configuration to map `Platform` entity to `GrpcPlatformModel`.
    - Registered the gRPC service in `Program.cs` using `builder.Services.AddGrpc()` and `app.MapGrpcService<GrpcPlatformService>()`.
    - Added a route to serve the `platforms.proto` file via `/protos/platforms.proto`.

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.AsyncDataServices;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Grpc;
 using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,6 +26,8 @@ builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 
 builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>();
+
+builder.Services.AddGrpc();
 
 builder.Services.AddControllers();
 
@@ -52,6 +55,10 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapGrpcService<GrpcPlatformService>();
+app.MapGet("/protos/platforms.proto", async context => 
+    await context.Response.WriteAsync(File.ReadAllText("Protos/platforms.protp"))
+);
 
 // Call the PrepPopulation method to initialize the database with seed data
 // This ensures that the database is populated with initial data when the application starts
