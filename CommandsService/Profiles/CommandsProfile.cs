b44c765322020583891d96728aae39b45d49f14b
unknown <basdemir35@hotmail.com.tr>
2024-08-27 16:54:28 +0300

    Add GRPC client for Platform data retrieval
    - Created `SyncDataServices/Grpc` folder inside `CommandsService`.
    - Added `IPlatformDataClient` interface to define the method for retrieving platforms via GRPC.
    - Implemented `PlatformDataClient` class, which calls the GRPC service to retrieve all platforms and maps the GRPC response to the `Platform` model.
    - Registered `IPlatformDataClient` and `PlatformDataClient` in the DI container within `Program.cs` using `AddScoped`.
    - Added AutoMapper configuration to map `GrpcPlatformModel` to `Platform`, mapping `PlatformId` to `ExternalID` and ignoring the `Commands` property.

--- CommandsService/Profiles/CommandsProfile.cs
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using CommandsService.Dtos;
 using CommandsService.Models;
+using PlatformService;
 
 namespace CommandsService.Profiles
 {
@@ -17,6 +18,11 @@ namespace CommandsService.Profiles
             // Map the Id property from PlatformPublishedDto to the ExternalID property in Platform
             CreateMap<PlatformPublishedDto, Platform>()
                 .ForMember(dest => dest.ExternalID, opt => opt.MapFrom(src => src.Id));
+            CreateMap<GrpcPlatformModel, Platform>()
+                .ForMember(dest => dest.ExternalID, opt => opt.MapFrom(src => src.PlatformId))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Commands, opt => opt.Ignore());
+
         }
     }
 }
